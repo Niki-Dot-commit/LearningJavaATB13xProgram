83723083a4e73f0294995cdf4a99c6ea57a3cd6f
Niki-dot-Commit <guptanikita477@gmail.com>
2025-06-12 21:11:41 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_05_Ternaryoperator/Lab_04EVEN_ODD.java
@@ -0,0 +1,9 @@
+package ex_05_Ternaryoperator;
+
+public class Lab_04EVEN_ODD {
+    public static void main(String[] args) {
+        int num=13;
+        String result = (num%2==0)? "even":"odd";
+        System.out.println(result);
+    }
+}
