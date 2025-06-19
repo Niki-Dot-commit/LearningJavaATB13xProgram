c2c10158563f82a014c1ac04ee0661c93cc3aa01
Niki-dot-Commit <guptanikita477@gmail.com>
2025-06-19 08:09:14 +0530

    question4

---
+++ src/task_examquestions/question4.java
@@ -0,0 +1,13 @@
+package task_examquestions;
+
+public class question4 {
+    public static void main(String[] args) {
+        int a =4 ;
+        System.out.println("Original: " + a);
+        System.out.println( " pre-incerement :" +(++a));
+        System.out.println( " post-incerement :" +(a++));
+        System.out.println( "pre-decerement :" +(--a));
+        System.out.println( " post-decerement :" +(a--));
+        System.out.println("final result: " + (a));
+    }
+}
