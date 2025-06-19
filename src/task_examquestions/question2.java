377e359e7dcc92c85d1e606b0a09b46d78e07c23
Niki-dot-Commit <guptanikita477@gmail.com>
2025-06-19 07:52:44 +0530

    question2

---
+++ src/task_examquestions/question2.java
@@ -0,0 +1,13 @@
+package task_examquestions;
+
+public class question2 {
+    public static void main(String[] args) {
+        int a =2;
+        int b =6 ;
+        System.out.println( "addition>" +(a + b));
+        System.out.println ("substraction>" +(b-a));
+        System.out.println("division>" +(a /b));
+        System.out.println("modulus>" +(a % b));
+        System.out.println("multiplication >" +(a * b));
+    }
+}
