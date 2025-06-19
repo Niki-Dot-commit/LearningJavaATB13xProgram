c76bb525e152e705dde23acd75d229026ec1ce6c
Niki-dot-Commit <guptanikita477@gmail.com>
2025-06-19 07:45:58 +0530

    question1

---
+++ src/task_examquestions/q1_.java
@@ -0,0 +1,18 @@
+package task_examquestions;
+
+public class q1_ {
+    public static void main(String[] args) {
+        int i = 1 ;
+        double d =2.4;
+        char c= 'a';
+        boolean correctstatement = true;
+        boolean falsestatement = false;
+        System.out.println( "integer:" +i);
+        System.out.println("double: " +d);
+        System.out.println("character: " +c);
+        System.out.println("Boolean (true): " + correctstatement);
+
+
+
+    }
+}
