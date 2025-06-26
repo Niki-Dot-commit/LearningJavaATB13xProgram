cdf7d38c01ac645b7d3e47923452e785814f6cf1
Niki-dot-Commit <guptanikita477@gmail.com>
2025-06-26 07:57:02 +0530

    UPDATE

---
+++ src/String/examq2.java
@@ -0,0 +1,16 @@
+package String;
+
+public class examq2 {
+    public static void main(String[] args) {
+        String string1="Hello";
+        String string2 ="hello";
+        String string3 ="Hello";
+        //compare strings in Java, including == operator,
+        // equals(), equalsIgnoreCase(), and compareTo() methods.
+        System.out.println(string1.equals("Hello"));
+        System.out.println(string2.equalsIgnoreCase("hello"));
+        System.out.println(string1.compareTo("Hello"));
+    }
+}
+
+
